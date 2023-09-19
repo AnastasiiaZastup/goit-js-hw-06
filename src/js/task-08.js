601039c1de102bbc19fea7d7de60845862d8711d
AnastasiiaZastup <zastupanastasia7@gmail.com>
2023-09-20 02:41:30 +0300

    done this

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -3,6 +3,25 @@ const elForm = document.querySelector('.login-form');
 
 elForm.addEventListener('submit', onsubmit);
 
+
 function onsubmit(evt) {
     evt.preventDefault();
+
+
+    const { email, password } = evt.currentTarget.elements;
+    
+    const data = {
+        email: email.value,
+        password: password.value
+    };
+
+    console.log(data);
+
+    if (!email.value || !password.value) {
+    alert('Please fill in all fields.');
+    }
+    
+    elForm.reset();
+    
 }
+
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const backgroundColor = document.querySelector('.color');
+const buttonColor = document.querySelector('.change-color');
+
+function changesColor() {
+  const randomColor = getRandomHexColor();
+  backgroundColor.style.backgroundColor = randomColor;
+}
+
+buttonColor.addEventListener('click', changesColor);
